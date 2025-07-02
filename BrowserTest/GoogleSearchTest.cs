c809a559809a7ee7e6a5ec172de177076c43805e
Drizmi <michael.drizga@gmail.com>
2025-07-02 10:15:47 +0200

    Project init

---
+++ BrowserTest/GoogleSearchTest.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace GoogleSearchTest;
+{
+    [TestFixture]
+    public class GoogleSearchTest
+    {
+        private IWebDriver driver;
+
+        [Setup]
+        public void Setup()
+        {
+            driver = new ChromeDriver();
+        }
+
+        [Test]
+        public void GoogleTitleCheck()
+        {
+            driver.Navigate().GoToUrl("https://www.google.com");
+            Assert.assertEqual("Google", driver.Title);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Quit();
+        }
+    }
+}
